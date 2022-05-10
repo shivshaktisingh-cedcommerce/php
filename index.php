65e7eed3b21f62fc4652add175dcef585684c4c8
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-10 18:15:43 +0530

    program 5 to find percentage

--- index.php
+++ index.php
@@ -19,3 +19,15 @@ echo "<br>";
 $str1="Hello world!";
 $str2="Good morning !";
 echo $str1 . " " . $str2;
+
+
+echo"<br>";
+$a=45;
+if($a>=60)
+echo "First Division";
+else if($a>=45 && $a<=59)
+echo "Second Division";
+else if($a>=33 && $a<=44)
+echo "third division";
+else
+echo "fail";
