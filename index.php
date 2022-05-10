be920914416ad1a4344f7b359d1bdd4bf1c1388d
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-10 18:25:22 +0530

    switch

--- index.php
+++ index.php
@@ -31,3 +31,32 @@ else if($a>=33 && $a<=44)
 echo "third division";
 else
 echo "fail";
+
+echo "<br>";
+$day = 3;
+
+switch ($day) {
+  case 1:
+    echo "Monday";
+    break;
+  case 2:
+     echo "Tuesday";
+    break;
+  case 3:
+     echo "Wednesday";
+    break;
+  case 4:
+    echo "Thursday";
+    break;
+  case 5:
+    echo "Thursday";
+    break;
+  case 6:
+    echo "friday";
+    break;
+  case 7:
+    echo "Saturday";
+    break;
+    default:
+    echo "number greater than 7";
+}
