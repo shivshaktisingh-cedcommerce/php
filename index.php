f4444d8316c22a309db64f0bd4eadb1700334e72
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-11 14:44:24 +0530

    required temperature,average temperature

--- index.php
+++ index.php
@@ -1,30 +1,38 @@
-<html>
-  <head>
-</head>
-<body>
-<table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
-
 <?php
 
+
 echo "<br>";
-echo "<br>";
-for($a=1;$a<=8;$a++)
-{
-  echo "<tr>";
-  for($b=1;$b<=8;$b++)
-  {  $Total=$a+$b;
-    if($Total%2==0)
-    {
-    echo"<td width=30px height=30px bgcolor=#FFFFFF </td>";
-    }
-    else
-    {
-      echo"<td width=30px height=30px bgcolor=#000000 </td>";
-    }
-  }
-  echo "</tr>";
+
+$original = array("1","2","3","4","5");
+foreach($original as $x){
+  echo " $x";
 }
+
+echo "<br>";
+$a="$";
+array_splice($original , 3,0 ,$a);
+foreach($original as $x){
+  echo " $x";
+}
+
+echo "<br>";
+
+$recordedtemp = array( 78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+$sum=array_sum($recordedtemp);
+$count = count($recordedtemp);
+$average = $sum/$count;
+echo "Average Temperature is: $average";
+echo "<br>";
+sort($recordedtemp);
+echo "<br>";
+echo "List of Seven Highest Temperature is: ";
+print_r(array_slice($recordedtemp, $count-7,$count));
+echo "<br>";
+echo "List of Seven Lowest Temperature is: ";
+print_r(array_slice($recordedtemp, 0, 7));
+echo "<br>";
+
+
+
+
 ?>
-</table>
-</body>
-</html>
