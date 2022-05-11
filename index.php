e93dedc16e80717ce1d7d8a5450fca7eaba1de59
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-11 13:40:59 +0530

    chessboard

--- index.php
+++ index.php
@@ -1,81 +1,30 @@
+<html>
+  <head>
+</head>
+<body>
+<table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+
 <?php
-for($a=5;$a<16;$a++)
+
+echo "<br>";
+echo "<br>";
+for($a=1;$a<=8;$a++)
 {
-    echo $a;
-    echo "<br>";
+  echo "<tr>";
+  for($b=1;$b<=8;$b++)
+  {  $Total=$a+$b;
+    if($Total%2==0)
+    {
+    echo"<td width=30px height=30px bgcolor=#FFFFFF </td>";
+    }
+    else
+    {
+      echo"<td width=30px height=30px bgcolor=#000000 </td>";
+    }
+  }
+  echo "</tr>";
 }
-
-$a=5;
-$b=6;
-$z=$a+$b;
-echo "<br>";
-echo $z;
-
-
-$a= "World";
-echo "<br>";
-echo "Welcome to the php $a";
-echo "<br>";
-$str1="Hello world!";
-$str2="Good morning !";
-echo $str1 . " " . $str2;
-
-
-echo"<br>";
-$a=45;
-if($a>=60)
-echo "First Division";
-else if($a>=45 && $a<=59)
-echo "Second Division";
-else if($a>=33 && $a<=44)
-echo "third division";
-else
-echo "fail";
-
-echo "<br>";
-$day = 3;
-
-switch ($day) {
-  case 1:
-    echo "Monday";
-    break;
-  case 2:
-     echo "Tuesday";
-    break;
-  case 3:
-     echo "Wednesday";
-    break;
-  case 4:
-    echo "Thursday";
-    break;
-  case 5:
-    echo "Thursday";
-    break;
-  case 6:
-    echo "friday";
-    break;
-  case 7:
-    echo "Saturday";
-    break;
-    default:
-    echo "number greater than 7";
-}
-
-
-echo "<br>";
-$a=1;
-for($i=1;$i<5;$i++)
-{
-    $a=$a*$i;
-}
-echo $a;
-
-
-echo "<br>";
-$original = array("1","2","3","4","5");
-$a=7;
-array_splice($original , 3,0,$a);
-foreach($original as $x){
-    echo "$x ";
-    
-}
+?>
+</table>
+</body>
+</html>
