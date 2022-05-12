fc73f22ee23244a2de1fa72df2e1b634e4fae8b8
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-12 17:41:13 +0530

    image upload

--- index.php
+++ index.php
@@ -1,49 +1,12 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
   <style>
     #outer{
       width:100%;
       height:100%;
-      
+     
     }
-    #inner{
-      width:50%;
-      height:40%;
-      margin:auto;
-      margin-top:10%;
-      background-color:powderblue;
-      padding:20px;
-      
-    }
-    input[type=number]{
-  width: 60%;
-  padding: 12px 20px;
-  margin: 28px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-left:80px;
-}
-#convert{
-  width: 60%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-left:80px;
-}
-#result{
-  margin-top:10%;
-  margin-left:20%;
-}
-input[type=radio]{
-  margin-left:20%;
-}
-
 
    
     </style>
@@ -52,25 +15,16 @@ input[type=radio]{
 <body>
   <?php
   
-  if(isset($_POST['htm']))
+  if(isset($_POST['img_submit']))
   { 
-   $operation = $_POST['htm'];
-   $numtoconvert = $_POST['insert'];
-   if(!empty($numtoconvert))
-   {
-     if($operation == 'htm'){ 
-       $a= "hours";
-       $b= "minutes";
-       $result = $numtoconvert * 60;
-      }
-      else{
-        $a="hours";
-        $b="seconds";
-        $result = $numtoconvert * 60 * 60;
-      }
-    
-     
-   }
+    echo "j";
+   $img_name = $_FILES['img_upload']['name'];
+   $tmp_img_name = $_FILES['img_upload']['tmp_name'];
+   $folder='upload/';
+   echo $folder.$img_name;
+   move_uploaded_file($tmp_img_name,$folder.$img_name);
+   //$folder='upload/';
+  
   }
 
 
@@ -78,30 +32,12 @@ input[type=radio]{
 
   <div id="outer">
     <div id="inner">
-  <form method="post" action ="">
+  <form method="post" action ="" enctype="multipart/form-data">
+    <input type="file" name ="img_upload">
+    <input type="submit" name ="img_submit">
+</form> 
  
-  <input type="number" id="typetoconvert" name="insert">
-  <br>
-  <br>
-  <input type="radio" id="htm" name="htm" value="htm">
-  <label for="htm">hour to mins</label><br><br>
-  <input type="radio" id="mts" name="htm" value="mts">
-  <label for="mts">minutes to second</label>
-  <div id="result">
-    <?php
-     echo "$numtoconvert  $a = $result   $b"; 
-    ?>
-    </div>
-    <br>
-    <br>
- 
-  <input type="submit"  id="convert" name="convert" value="Convert">
-  <br>
-  <br>
   
-  
-  <br>
-  <br>
   
 </div>
 </div>
