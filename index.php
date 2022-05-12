f599c8ea29b109c7eff26cb245cf927f6ffb051a
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-12 10:17:09 +0530

    for each multidimensional array

--- index.php
+++ index.php
@@ -1,52 +1,109 @@
+
 <?php
-function here_to_delete($var){
-  return $var !== 4;
-  //return ($var != $value);
-}
+$products = array(
+  "Electronics" => array(
+    "Television" => array(
+      array(
+      "id" => "PR001",
+      "name" => "MAX-001",
+      "brand" => "Samsung"
+      ),
+      array(
+      "id" => "PR002",
+      "name" => "BIG-301",
+      "brand" => "Bravia"
+      ),
+      array(
+      "id" => "PR003",
+      "name" => "APL-02",
+      "brand" => "Apple"
+      )
+    ),
+    "Mobile" => array(
+      array(
+      "id" => "PR004",
+      "name" => "GT-1980",
+      "brand" => "Samsung"
+      ),
+      array(
+      "id" => "PR005",
+      "name" => "IG-5467",
+      "brand" => "Motorola"
+      ),
+      array(
+      "id" => "PR006",
+      "name" => "IP-8930",
+      "brand" => "Apple"
+      )
+    )
+  ),
+  "Jewelry" => array(
+    "Earrings" => array(
+      array(
+      "id" => "PR007",
+      "name" => "ER-001",
+      "brand" => "Chopard"
+      ),
+      array(
+      "id" => "PR008",
+      "name" => "ER-002",
+      "brand" => "Mikimoto"
+      ),
+      array(
+      "id" => "PR009",
+      "name" => "ER-003",
+      "brand" => "Bvlgari"
+      )
+    ),
+    "Necklaces" => array(
+      array(
+      "id" => "PR010",
+      "name" => "NK-001",
+      "brand" => "Piaget"
+      ),
+      array(
+      "id" => "PR011",
+      "name" => "NK-002",
+      "brand" => "Graff"
+      ),
+      array(
+      "id" => "PR012",
+      "name" => "NK-003",
+      "brand" => "Tiffany"
+      )
+    )
+  )
+      );
 
-echo "<br>";
 
-$original = array("1","2","3","4","5");
-foreach($original as $x){
-  echo " $x";
-}
+echo "<table width=50% height=50% border=black><tr><td>Category</td><td>subCategory</td><td>ID</td><td>Name</td><td>Brand</td></tr>";
+foreach($products as $i => $values)//electronic,jwelry
+  {  
+    
+   
+      {
+      foreach($values as $subkey => $subvalue)
+        {
+         
+        
+         foreach($subvalue as $key => $subsubvalue)
+         {
+         echo("<tr>");
+         echo "<td>$i</td>";
+         echo "<td>$subkey</td>";
+       
+         
+           foreach($subsubvalue as $keyin => $subsubsubvalue)
+           {
+            echo("<td>$subsubsubvalue</td>");
+           }
+           echo "</tr>";
+        }
+      }
+        
+  }             
+           
+   
 
-echo "<br>";
-$a="$";
-array_splice($original , 3,0 ,$a);
-foreach($original as $x){
-  echo " $x";
-}
-
-echo "<br>";
-
-$recordedtemp = array( 78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
-$sum=array_sum($recordedtemp);
-$count = count($recordedtemp);
-$average = $sum/$count;
-echo "Average Temperature is: $average";
-echo "<br>";
-sort($recordedtemp);
-echo "<br>";
-echo "List of Seven Highest Temperature is: ";
-print_r(array_slice($recordedtemp, $count-7,$count));
-echo "<br>";
-echo "List of Seven Lowest Temperature is: ";
-print_r(array_slice($recordedtemp, 0, 7));
-echo "<br>";
-
-
-
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-echo $age['Peter'];
-echo "<br>";
-
-
-$age = array(1,2,3,4,5,6,7);
-$value= 4;
-print_r($age);
-echo "<br>";
-print_r(array_filter($age,"here_to_delete"));
-
-
-?>
+   
+ }
