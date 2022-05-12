a0f865b55363210fc56b240c2cd240fe41b63334
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-12 13:05:42 +0530

    electricity bill

--- index.php
+++ index.php
@@ -1,109 +1,72 @@
+<html>
+  <head>
+  
+      
+  </head>
+  <body>
 
-<?php
-$products = array(
-  "Electronics" => array(
-    "Television" => array(
-      array(
-      "id" => "PR001",
-      "name" => "MAX-001",
-      "brand" => "Samsung"
-      ),
-      array(
-      "id" => "PR002",
-      "name" => "BIG-301",
-      "brand" => "Bravia"
-      ),
-      array(
-      "id" => "PR003",
-      "name" => "APL-02",
-      "brand" => "Apple"
-      )
-    ),
-    "Mobile" => array(
-      array(
-      "id" => "PR004",
-      "name" => "GT-1980",
-      "brand" => "Samsung"
-      ),
-      array(
-      "id" => "PR005",
-      "name" => "IG-5467",
-      "brand" => "Motorola"
-      ),
-      array(
-      "id" => "PR006",
-      "name" => "IP-8930",
-      "brand" => "Apple"
-      )
-    )
-  ),
-  "Jewelry" => array(
-    "Earrings" => array(
-      array(
-      "id" => "PR007",
-      "name" => "ER-001",
-      "brand" => "Chopard"
-      ),
-      array(
-      "id" => "PR008",
-      "name" => "ER-002",
-      "brand" => "Mikimoto"
-      ),
-      array(
-      "id" => "PR009",
-      "name" => "ER-003",
-      "brand" => "Bvlgari"
-      )
-    ),
-    "Necklaces" => array(
-      array(
-      "id" => "PR010",
-      "name" => "NK-001",
-      "brand" => "Piaget"
-      ),
-      array(
-      "id" => "PR011",
-      "name" => "NK-002",
-      "brand" => "Graff"
-      ),
-      array(
-      "id" => "PR012",
-      "name" => "NK-003",
-      "brand" => "Tiffany"
-      )
-    )
-  )
-      );
+  <?php
+  if(isset($_POST['unit_submit']))
+  {
+    $units = $_POST['units'];
+    if(!empty($units)){
+      $result = calculate_bill($units);
+    }
+  }
+  function calculate_bill($units)
+  {
+    if($units<50)
+    {
+      $bill=3.50 * $units;
+    }
+    else if($units>50 && $units<150)
+    {
+       $temp=50 * 3.50; 
+       $bill= $temp + (4 * ($units-50));
+    }
+    else if($units>150 && $units<250)
+    { 
+       $temp=(50 * 3.50) + (100 *4);
+       $bill= $temp + (5.20 * ($units -150));
+    }
+    else if($units>250)
+    {
+       $temp=(50 * 3.50) + (100 * 4) + (100 * 5.20);
+       $bill = $temp + (($units-250)*6.50);;
+    }
+    return $bill;
+  }
+  ?>
+  <div id="outer" style>
+    <div id="bill">
+    <form method="post" action="">
+      <input type="number" name="units" id="units" placeholder="Enter Unit Here">
+      <input type="submit" name="unit_submit" id="unit-submit" value="submit">
+    </form>
+    </div>
+    <div id="result">
+      <?php
+      echo "your electricity bill is $result";
+      ?>
+</div>
 
-
-echo "<table width=50% height=50% border=black><tr><td>Category</td><td>subCategory</td><td>ID</td><td>Name</td><td>Brand</td></tr>";
-foreach($products as $i => $values)//electronic,jwelry
-  {  
-    
-   
-      {
-      foreach($values as $subkey => $subvalue)
-        {
-         
-        
-         foreach($subvalue as $key => $subsubvalue)
-         {
-         echo("<tr>");
-         echo "<td>$i</td>";
-         echo "<td>$subkey</td>";
-       
-         
-           foreach($subsubvalue as $keyin => $subsubsubvalue)
-           {
-            echo("<td>$subsubsubvalue</td>");
-           }
-           echo "</tr>";
-        }
-      }
-        
-  }             
-           
-   
-
-   
- }
+</div>
+  <style>
+    #outer {
+      width:100%;
+      height:100%;
+      background-color:lightgray;
+      float:left;
+    }
+    #bill {
+      margin-top:100px;
+      margin-left:200px;
+      
+    }
+    #result {
+      margin-top:10px;
+      margin-left:200px;
+    }
+  </style>
+  </body>
+</html>
