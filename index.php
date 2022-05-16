5b1cb6ce8784f2ffc6d0711aae44d9c07a94d758
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-16 10:45:27 +0530

    session image array

--- index.php
+++ index.php
@@ -1,47 +1,59 @@
- <!DOCTYPE html>
-<html>
-<head>
-  <style>
-    #outer{
-      width:100%;
-      height:100%;
-     
-    }
+<?php
+session_start();
 
-   
-    </style>
-  <title>Document</title>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <?php
-  
-  if(isset($_POST['img_submit']))
-  { 
-    echo "j";
-   $img_name = $_FILES['img_upload']['name'];
-   $tmp_img_name = $_FILES['img_upload']['tmp_name'];
-   $folder='upload/';
-   echo $folder.$img_name;
-   move_uploaded_file($tmp_img_name,$folder.$img_name);
-   //$folder='upload/';
-  
-  }
-
-
-  ?>
-
-  <div id="outer">
-    <div id="inner">
-  <form method="post" action ="" enctype="multipart/form-data">
-    <input type="file" name ="img_upload">
-    <input type="submit" name ="img_submit">
-</form> 
- 
-  
-  
-</div>
-</div>
+<form method="POST" action="" enctype="multipart/form-data">
+    <?php
+    if(isset($_POST['sessionDestroy']))
+    {
+        session_destroy();
+    }
+    if(isset($_POST['img_submit']))
+    {
+        $img_name=$_FILES['img_upload']['name'];
+        
+        $tmp_img_name=$_FILES['img_upload']['tmp_name'];
+        $folder ='upload/';     
+        if(move_uploaded_file($tmp_img_name,$folder.$img_name))
+        {
+            
+            $_SESSION['img1'][]=$img_name;
+        }
     
   
+    ?>
+    <div class="div" style="display:flex;justify-content:center">
+
+       <?php if(isset($_SESSION['img1'])){for($i=0;$i<count($_SESSION['img1']);$i++){?>
+        <div>
+            <img src="upload/<?php echo $_SESSION['img1'][$i]?>" style="height:300px;width:300px" alt="">
+            <p><?php echo $_SESSION['img1'][$i]?></p>
+       </div>
+       <?php } }?>
+        </div>
+    <?php
+    }
+    ?>
+      <input type="file" name="img_upload"><br><br>
+      <input type="submit" name="img_submit">   
+      <input type="submit" name="sessionDestroy"> 
+</form>
+<?php
+
+?>
+<?php
+
+  ?>  
 </body>
 </html>
+
